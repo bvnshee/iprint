54319dbef62bfab2ef14cbb9422e9c6a3171d2b1
bvnshee <kukarekusyka@mail.ru>
2021-01-29 17:34:00 +0500

    init

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+var slider = tns({
+  container: ".promo__slider",
+  items: 1,
+  swipeAngle: false,
+  arrowKeys: true,
+  speed: 2000,
+  autoplayTimeout: 4000,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  controlsText: [
+    '<img src="../icons/arrow-left.svg">',
+    '<img src="../icons/arrow-right.svg">',
+  ],
+});
